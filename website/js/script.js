8177d05b86041d521e3ddd2884da2045e3c76bd5
Kseniya Kravchuk <k.kravchuk@hpmdnetwork.ru>
2019-11-15 20:06:31 +0300

    Added some edits

--- website/js/script.js
+++ website/js/script.js
@@ -26,6 +26,14 @@
 		}
 
 		waveAnimation()
+
+        var animation = bodymovin.loadAnimation({
+            container: document.getElementById('bm'),
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            path: 'data.json'
+        })
 	});
 })();
 
