1ef651c8f81e70d44072bdde77870b4d59241fcc
Kseniya Kravchuk <k.kravchuk@hpmdnetwork.ru>
2019-11-25 18:13:13 +0300

    Added parallax on views page

--- Mini_Project/src/js/script.js
+++ Mini_Project/src/js/script.js
@@ -1,8 +1,21 @@
-function parallaxEffect(event) {
+function parallaxEffectScroll() {
+    document.querySelectorAll('.parallax-item').forEach(function(item) {
+        var speed = item.getAttribute('data-speed');
+        item.style.transform = 'translateY(' + pageYOffset*speed/600 + 'px)';
+    })
+}
+
+function parallaxEffectMove(event) {
     document.querySelectorAll('.parallax-item').forEach(function(item) {
         var speed = item.getAttribute('data-speed');
         item.style.transform = 'translate(' + event.clientX*speed/1000 + 'px,' + event.clientY*speed/1000 + 'px)';
     })
 }
 
-document.addEventListener('mousemove', parallaxEffect);
+if (window.location.href.indexOf('index') > -1) {
+    document.removeEventListener('scroll', parallaxEffectScroll);
+    document.addEventListener('mousemove', parallaxEffectMove);
+} else if (window.location.href.indexOf('views') > -1) {
+    document.removeEventListener('mousemove', parallaxEffectMove);
+    document.addEventListener('scroll', parallaxEffectScroll);
+}
