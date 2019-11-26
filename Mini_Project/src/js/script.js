815b7a9336d0f8a3d90c7757c263f39f452830b8
Kseniya Kravchuk <k.kravchuk@hpmdnetwork.ru>
2019-11-26 12:28:47 +0300

    Added parallax for touchmove

--- Mini_Project/src/js/script.js
+++ Mini_Project/src/js/script.js
@@ -12,10 +12,19 @@ function parallaxEffectMove(event) {
     })
 }
 
+function parallaxEffectTouchMove(event) {
+    document.querySelectorAll('.parallax-item').forEach(function(item) {
+        var speed = item.getAttribute('data-speed');
+        item.style.transform = 'translate(' + event.touches[0].clientX*speed/1000 + 'px,' + event.touches[0].clientY*speed/1000 + 'px)';
+    })
+}
+
 if (window.location.href.indexOf('index') > -1) {
     document.removeEventListener('scroll', parallaxEffectScroll);
     document.addEventListener('mousemove', parallaxEffectMove);
+    document.addEventListener('touchmove', parallaxEffectTouchMove);
 } else if (window.location.href.indexOf('views') > -1) {
     document.removeEventListener('mousemove', parallaxEffectMove);
+    document.removeEventListener('touchmove', parallaxEffectTouchMove);
     document.addEventListener('scroll', parallaxEffectScroll);
 }
