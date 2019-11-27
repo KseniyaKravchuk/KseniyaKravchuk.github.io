9aa6bc1bdc7546c5d4e9c55931fb0f88f4dfbad4
Kseniya Kravchuk <k.kravchuk@hpmdnetwork.ru>
2019-11-27 18:55:05 +0300

    Added immersion template

---
+++ my_vue_project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your App"/>
+    <TextBox name="Jone" msg="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+    <TextBox name="Bob" msg="Animi consequuntur delectus nesciunt, quae sint temporibus!"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TextBox from "./components/TextBox";
+
+export default {
+  name: 'app',
+  components: {
+    TextBox,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my_vue_project/src/components/TextBox.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Hello {{ name }}!</h1>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TextBox',
+        props: {
+            msg: String,
+            name: String
+        }
+    }
+</script>
+
+<style>
+
+</style>
